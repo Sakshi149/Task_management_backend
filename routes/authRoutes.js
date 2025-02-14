5b669894bfc698762c83326c1751341b6a2c4cf2
Sakshi149 <chavan06sakshi@gmail.com>
2025-02-14 14:49:49 +0530

    Provided Apis for pages

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { register, login } from '../controllers/authController.js';
+import { register, login, logout, getUserInfo, changePassword } from '../controllers/authController.js';
+import authenticateToken from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/register', register); // Register a new user
-router.post('/login', login);       // Log in and get a token
+router.post('/login', login); // Log in and get a token
+router.get('/logout', logout); // Log out and get a token
+router.get("/profile", getUserInfo); // profile retrived from token
+router.post("/change-password", authenticateToken, changePassword)
 
 export default router;
