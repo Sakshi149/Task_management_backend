9bbf2e469eb1cdea34a0ce3b97d1e025d337406e
Sakshi149 <chavan06sakshi@gmail.com>
2025-02-14 14:38:45 +0530

    Added try catch block

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -7,13 +7,22 @@ const authenticateToken = (req, res, next) => {
   }
 
   const token = authHeader.split(' ')[1];
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) {
-      return res.status(403).json({ error: 'Forbidden: Invalid or expired token' });
-    }
-    req.user = user;
+  
+  // jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+  //   if (err) {
+  //     return res.status(403).json({ error: 'Forbidden: Invalid or expired token' });
+  //   }
+  //   req.user = user;
+  //   next();
+  // });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; 
     next();
-  });
+  } catch (error) {
+    return res.status(403).json({ error: "Invalid or expired token." });
+  }
 };
 
 export default authenticateToken;
